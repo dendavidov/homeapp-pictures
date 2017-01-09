276c63bd5704b78d30212cdcb115eb1135d5cd0f
dendavidov <mail@dendavidov.com>
2017-01-10 00:00:44 +0300

    server. add bcrypt-thunk

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const co = require('co');
-const bcrypt = require('bcrypt');
+const bcrypt = require("../lib/bcrypt-thunk");
 
 const UserSchema = new Schema({
   username: {
