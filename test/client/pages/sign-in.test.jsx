0b8d77ea2fe4c41d40063c9b8230d2e0af2620d2
DavDA <Denis.Davydov@bssys.com>
2017-02-14 09:58:09 +0300

    fix test for SignIn component

--- test/client/pages/sign-in.test.jsx
+++ test/client/pages/sign-in.test.jsx
@@ -4,6 +4,12 @@ import renderer from 'react-test-renderer';
 import SignIn from '../../../src/client/pages/sign-in';
 
 describe('SignIn component is rendered correctly', () => {
+  beforeEach(() => {
+    SignIn.contextTypes = {
+      router: () => {},
+    };
+  });
+
   it('OK', () => {
     const rendered = renderer.create(
       <SignIn />,
